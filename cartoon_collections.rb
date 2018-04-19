2b1370f0158e6efda51faf5787b101960a4359a5
Brent Bauman <brentbauman86@gmail.com>
2018-04-19 15:42:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end
 
 def find_the_cheese(strings)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.find {|types| 
+  cheese_types.find {|types|
   if strings.include?(cheese_types)
     true
   else
