443ab949ce8f7ea8f50425d004bc04d34a5e6f49
Brent Bauman <brentbauman86@gmail.com>
2018-04-19 13:46:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  new_array = []
+  names.each_with_index {|name, index| new_array << puts "#{name} #{index}"}
+return new_array
 end
 
 def summon_captain_planet# code an argument here
