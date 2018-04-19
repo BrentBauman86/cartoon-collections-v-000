5eb3e17f3b1978a7160040e725e0ae2a99f01451
Brent Bauman <brentbauman86@gmail.com>
2018-04-19 14:16:11 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@
 #binding.pry
 def roll_call_dwarves(names)
   new_array = []
-  names.each_with_index {|name, index| new_array << "#{name} #{index}"}
+  names.each_with_index[1] {|name, index| new_array << "#{name} #{index[1]}"}
 return new_array
 end
 
